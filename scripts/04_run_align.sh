2eae29bb0824132caaa4a1ec905fe4825db4b949
ryanpeek <rapeek@ucdavis.edu>
2020-11-10 14:27:15 -0800

    lord help me

--- scripts/04_run_align.sh
+++ scripts/04_run_align.sh
@@ -13,7 +13,7 @@ set -x
 #  	sbatch --mem MaxMemPerNode 05a_align.sh 3_col.bamlist reference_alignment.fa
 
 ## assume running script while located IN dir with fqs, make a bamlist
-list=$(ls *RA* > list_A; ls *RB* > list_B; ls *RA* | sed "s/\_RA//g" | sed "s/\.fastq//g" > list_noAB; paste list_*> align_list)
+list=$(ls *RA* > list_A; ls *RB* > list_B; ls *RA* | sed "s/\_RA//g" | sed "s/\.fastq//g" > list_noAB; paste list_* )
 
 #list=$1 # give your bamlist of 3 cols RA RB output name 
 ref=$1 # give reference alignment
