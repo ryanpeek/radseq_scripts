ee2df201d77a379fa11987802ac9691e475c96a4
Ryan Peek <rapeek@ucdavis.edu>
2021-04-16 12:54:18 -0700

    fix header

--- scripts/02_make_novoseq_split_script_indiv.sh
+++ scripts/02_make_novoseq_split_script_indiv.sh
@@ -22,27 +22,27 @@ while [ $x -le $wc ]
 	       
 	       # set up script header
 	       
-	       echo "
-	       #!/bin/bash -l
+	       echo "#!/bin/bash -l
 	       
-	       #SBATCH --mail-user=rapeek@ucdavis.edu
-	       #SBATCH --mail-type=ALL
-	       #SBATCH -J splitNovo
-	       #SBATCH -e splitNovo.%j.err
-	       #SBATCH -o splitNovo.%j.out
-	       #SBATCH -c 20
-	       #SBATCH -p high
-	       #SBATCH --time=1-20:00:00
+#SBATCH --mail-user=rapeek@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J splitNovo
+#SBATCH -e splitNovo.%j.err
+#SBATCH -o splitNovo.%j.out
+#SBATCH -c 20
+#SBATCH -p high
+#SBATCH --time=1-20:00:00
 	       
-	       #set -v # verbose -- all lines
-	       #set -x # trace of all commands before execution
+#set -v # verbose -- all lines
+#set -x # trace of all commands before execution
 	       
-	       seq1=\$1 # fq 1/A
-	       seq2=\$2 # fq 2/B
-	       somm=\$3 # name of SOMM
+seq1=\$1 # fq 1/A
+seq2=\$2 # fq 2/B
+somm=\$3 # name of SOMM
 	       
-	       grep --no-group-separator -A 3 \":${str}\" \${seq1} > ../fastq/\${somm}_${str}_R1.fastq
-	       grep --no-group-separator -A 3 \":${str}\" \${seq2} > ../fastq/\${somm}_${str}_R2.fastq" > ${outdir}/split_novoseq_${str}.sh
+grep --no-group-separator -A 3 \":${str}\" \${seq1} > ../fastq/\${somm}_${str}_R1.fastq
+grep --no-group-separator -A 3 \":${str}\" \${seq2} > ../fastq/\${somm}_${str}_R2.fastq" > ${outdir}/split_novoseq_${str}.sh
+
 	       x=$(( $x + 1 ))
 	       
        done
